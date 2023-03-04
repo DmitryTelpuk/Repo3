53aa1ad2a7be8d8356e1ee9cb2e5f05b3f19df45
dima <dima.telpuk@gmai.com>
2023-03-04 14:25:56 +0300

    Initial Commit

---
+++ Repo3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Repo3
+//
+//  Created by Dima Telpuk on 4.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
